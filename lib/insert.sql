bf69af6d8d3b61b8dc4cd6590bc2e473858d1da7
Rwanda S <rwanda.ace@gmail.com>
2020-06-20 18:25:56 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,14 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (NULL, null, null, null, null);
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null, null, null, null);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null,null,null,null);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null,null,null,null);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null,null,null,null);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null,null,null,null);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null,null,null,null);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null,null,null,null);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null,null,null,null);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null,null,null,null);
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null,null,null,null);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null, null, null, null);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null, null, null, null);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null, null, null, null);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null, null, null, null);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null, null, null, null);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null, null, null, null);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null, null, null, null);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES (null, null, null, null, null);
+
 INSERT INTO users (name, age) VALUES (null,null);
 INSERT INTO users (name, age) VALUES (null,null);
 INSERT INTO users (name, age) VALUES (null,null);
